119df6a3b68813024980b3b7ce20fe729f9ed434
Michael Gira <michaelgira23@gmail.com>
2017-06-23 22:27:38 -0500

    Work on USB Streams

--- src/drivers/usbstreams.ts
+++ src/drivers/usbstreams.ts
@@ -3,8 +3,8 @@
  */
 
 import { Readable, Writable } from 'stream';
-
-// import * as usb from 'usb';
+import * as usb from 'usb';
+import { InEndpointWithOn, OutEndpointWithOn } from '../endpoints';
 
 /**
  * Stream from dongle --> PC
@@ -12,23 +12,34 @@ import { Readable, Writable } from 'stream';
 
 export class InStream extends Readable {
 
-	/*
 	constructor(private endpoint: usb.InEndpoint) {
 		super();
+		(this.endpoint as InEndpointWithOn).on('data', this.onData.bind(this));
+		(this.endpoint as InEndpointWithOn).on('error', this.onError.bind(this));
+		(this.endpoint as InEndpointWithOn).on('end', this.onEnd.bind(this));
+	}
+
+	_read(size: number) {
+		console.log('InStream - Read:', size);
 	}
 
 	private onData(chunk: Buffer) {
-		//
+		console.log('InStream - OnData:', chunk);
+		if (!this.push(chunk)) {
+			// Disable line because callback is required, but we don't need anything in it (?)
+			// tslint:disable-next-line:no-empty
+			this.endpoint.stopPoll(() => {});
+		}
 	}
 
 	private onError(err: Error) {
-		//
+		console.log('InStream - OnError:', err);
+		this.emit('error', err);
 	}
 
 	private onEnd() {
-		//
+		console.log('InStream - OnEnd');
 	}
-	*/
 
 }
 
@@ -38,18 +49,23 @@ export class InStream extends Readable {
 
 export class OutStream extends Writable {
 
-	/*
 	constructor(private endpoint: usb.OutEndpoint) {
 		super();
+		(this.endpoint as OutEndpointWithOn).on('error', this.onError.bind(this));
+		(this.endpoint as OutEndpointWithOn).on('end', this.onEnd.bind(this));
 	}
 
-	private onError() {
-		//
+	_write(chunk: Buffer, encoding: string, callback: (err?: string) => void) {
+		this.endpoint.transfer(chunk, callback);
+	}
+
+	private onError(err: Error) {
+		console.log('OutStream - OnError:', err);
+		this.emit('error', err);
 	}
 
 	private onEnd() {
-		//
+		console.log('OutStream - OnEnd');
 	}
-	*/
 
 }
---
+++ src/endpoints.ts
@@ -0,0 +1,14 @@
+/**
+ * Because @types/usb doesn't have the `.on()` function for the endpoints
+ * @todo Make a PR to DefinitelyTyped to fix this for others
+ */
+
+import * as usb from 'usb';
+
+export interface InEndpointWithOn extends usb.InEndpoint {
+	on(event: string, callback: (device: usb.Device) => void): void;
+}
+
+export interface OutEndpointWithOn extends usb.OutEndpoint {
+	on(event: string, callback: (device: usb.Device) => void): void;
+}
