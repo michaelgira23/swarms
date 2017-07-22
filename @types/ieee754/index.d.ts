0f07fc8b57928d539ee7fc1278af787b015f491c
Michael Gira <michaelgira23@gmail.com>
2017-07-22 17:01:07 -0500

    Remove unused dependency

--- @types/ieee754/index.d.ts
+++
@@ -1,9 +0,0 @@
-// Type definitions for ieee754 1.1
-// Project: https://github.com/feross/ieee754
-// Definitions by: Michael Gira <https://github.com/michaelgira23>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-/// <reference types="node" />
-
-export function read(buffer: Buffer, offset: number, isLe: boolean, mLen: number, nBytes: number): number;
-export function write(buffer: Buffer, value: number, offset: number, isLe: boolean, mLen: number, nBytes: number): void;
